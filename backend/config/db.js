67b1defc03e724583bf1e9af680be4913577088d
nindyfaoziyah <nindyfaoziyah48@gmail.com>
2026-04-18 08:42:58 +0700

    progress lokal

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASS || '',
-    database: process.env.DB_NAME || 'attendify_db',
+    database: process.env.DB_NAME || 'db_absensi',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
